dd2dda51257919ae29e36441afc738993cac67cb
Widanta <widanta66@gmail.com>
2021-09-27 13:07:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// scroll top
+const scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrollTop.classList.add("active");
+  } else {
+    scrollTop.classList.remove("active");
+  }
+});
+// akhir scroll top
 
 
 
